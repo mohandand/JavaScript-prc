8f9d92c7ab520edd7a456d6c61c61a22fd5c2b14
mohan dandigam <mohandand@gmail.com>
2023-12-29 20:58:31 -0800

    coding-dsa

--- JS_DSA/LeetCode/uniChar.js
+++ JS_DSA/LeetCode/uniChar.js
@@ -4,6 +4,11 @@ function uniChar(str){
     obj[char] ? obj[char]++ : obj[char] =1
    })
    console.log(Object.values(obj));
-   return Object.values(obj).every((item) => item === 1);
+   for(let item in obj){
+    if(obj[item] === 1){
+        return item;
+    }
+   }
+   //return Object.values(obj).every((item) => item === 1);
 }
 console.log(uniChar('abcde'))
