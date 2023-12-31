3b1390f75af1a107f89cb2e97e1e94ed4d78bd7e
mohan dandigam <mohandand@gmail.com>
2023-12-30 21:25:34 -0800

    coding-dsa

--- JS_DSA/BinaryTree/comapreTree.js
+++ JS_DSA/BinaryTree/comapreTree.js
@@ -22,27 +22,16 @@ function isSame(p,q){
     return left&&right;
 }
 
-function isSameRec(p,q){
-    let queue = [p,q];
-    while(queue.length>0){
-        let node1 = queue.shift();
-        let node2 = queue.shift();
-
-        if(!node1 && !node2){
-            continue;
-        }
-
-        if((!node1 || !node2) && node1 !== node2){
-            return false;
-        }
-        queue.push(node1.left)
-        queue.push(node2.left)
-        queue.push(node1.right)
-        queue.push(node2.left)
+var isSameTree = function(p, q) {
+    const s = [[p,q]];
+    while (s.length) {
+        const [p,q] = s.pop();
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        if (p.val !== q.val) return false;
+        if (p.left || q.left) s.push([p.left, q.left]);
+        if (p.right || q.right) s.push([p.right, q.right]);
     }
-    return true
-}
-
-
-
+    return true;
+};
 console.log(isSameRec(a,a));
--- JS_DSA/BinaryTree/sample.js
+++ JS_DSA/BinaryTree/sample.js
@@ -9,3 +9,29 @@ class Node{
 
 let a = new Node('a');
 let b = new Node('b');
+let c = new Node('c');
+let d = new Node('d');
+let e = new Node('e');
+let f = new Node('f');
+
+a.left = b
+a.right= c;
+c.left = f;
+b.left = d;
+b.right = e;
+
+console.log(a)
+function inverttree(root){
+    if(!root) return [];
+    let stack = [root];
+    while(stack.length>0){
+        let current = stack.pop();
+        if(!current){
+            [current.left, current.right] = [current.right, current.left];
+            stack.push([current.left, current.right])
+        }
+    }
+    console.log(a)
+}
+
+console.log(inverttree(a));
