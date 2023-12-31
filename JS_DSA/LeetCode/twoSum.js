f75714e00bb59a61d9bb2f15949113284fa560ff
mohan dandigam <mohandand@gmail.com>
2023-12-31 15:48:30 -0800

    coding-dsa

--- JS_DSA/LeetCode/twoSum.js
+++ JS_DSA/LeetCode/twoSum.js
@@ -2,13 +2,11 @@ function twoSum(ar, k){
     let obj={};
     for(let i=0;i<ar.length;i++){
         let diff = k-ar[i];
-        if(obj[diff]){
+        if(obj[diff] !== undefined){
             return [obj[diff],i]
-        } else {
-            obj[diff] = i
-        }
+        } 
+        obj[ar[i]] =i
     }
     return -1;
 }
-
 console.log(twoSum([2,7,11,15],9))
