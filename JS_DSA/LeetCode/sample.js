b53259747545cfc21b219d0f1c8c49065e0d8525
mohan dandigam <mohandand@gmail.com>
2023-12-31 18:41:21 -0800

    coding-dsa

--- JS_DSA/LeetCode/sample.js
+++ JS_DSA/LeetCode/sample.js
@@ -1,22 +1,8 @@
-function adjRem(str,k){
-    let stack = [];
-    for(let i=0;i<str.length;i++){
-        let current=s[i];
-        let peek = stack[stack.length-1]
-        if( stack.length === 0 || peek[0] !== current){
-            stack.push([current, 1])
-        } else {
-            peek[1]++
-            if(peek[1]>k){
-                stack.pop();
-            }
-        }
-    }
-  let res =''
-    for(let [char, count] of stack){
-        res = res + char.repeat(count)
-    }
-    return res;
+function sortChr(str){
+    str.split(' ').sort((a,b) => {
+        let letterA = parseInt(a.match(/\d/));
+        let letterB = parseInt(b.match(/\d/));
+        return letterA-letterB
+    })
+    return str.join(' ')
 }
-
-console.log(adjRem("deeedbbcccbdaa",3))
