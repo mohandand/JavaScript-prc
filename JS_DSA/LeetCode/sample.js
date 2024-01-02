0eef698050a8c80f60b82270fd2b11dc4b2ed6ad
mohan dandigam <mohandand@gmail.com>
2024-01-01 18:47:18 -0800

    coding-dsa

--- JS_DSA/LeetCode/sample.js
+++ JS_DSA/LeetCode/sample.js
@@ -1,8 +1,15 @@
-function sortChr(str){
-    str.split(' ').sort((a,b) => {
-        let letterA = parseInt(a.match(/\d/));
-        let letterB = parseInt(b.match(/\d/));
-        return letterA-letterB
-    })
-    return str.join(' ')
+function lss(str){
+    let maxLength=0;
+    let char = new Set();
+    let left=0;
+    for(let i=0;i<str.length;i++){
+        while(char.has(str[i])){
+            char.delete(str[left]);
+            left++;
+        }
+        char.add(str[i]);
+        maxLength = Math.max(maxLength, char.size)
+    }
+    return maxLength;
 }
+console.log(lss("bbbbb"));
