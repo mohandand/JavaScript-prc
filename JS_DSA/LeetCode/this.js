7decdca94187ad4cc2ddb6df63f8f8506956733f
mohan dandigam <mohandand@gmail.com>
2024-01-02 11:51:34 -0800

    coding-dsa

--- JS_DSA/LeetCode/this.js
+++ JS_DSA/LeetCode/this.js
@@ -1,5 +1,12 @@
 function exc(){
-    console,log(this)
+    console.log(this)
 }
-console.log(this);
+//console.log(this);
 exc();
+
+myObj = {
+    name:"me",
+    fname:this.name,
+    iam:this
+}
+console.log(myObj)
