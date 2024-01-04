29b6ee5b28d58f8674c28ba46815a539bd65dfe6
mohan dandigam <mohandand@gmail.com>
2024-01-03 17:08:31 -0800

    JS_UI_Coding

---
+++ Javascript_Apps/Practice/script.js
@@ -0,0 +1,7 @@
+const divEle = document.querySelector('.sample');
+
+function changeBackGround(item){
+   //divEle.style.backgroundColor = "red";
+    divEle.innerText= item
+}
+console.log("sample")
