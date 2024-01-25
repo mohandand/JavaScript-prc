d00a2bf5f15298d18d82544bf468f0c18452d0b9
mohan dandigam <mohandand@gmail.com>
2024-01-25 12:27:03 -0800

    callback example

--- Javascript_Concepts/Promise/callback.js
+++ Javascript_Concepts/Promise/callback.js
@@ -1,18 +1,21 @@
 function readFile(cb){
     setTimeout(() => {
         console.log("Read File Compmpleted");
+        cb()
     },1000)
 }
 
 function processFile(cb){
     setTimeout(() => {
         console.log("processFile Compmpleted");
+        cb()
     },1000)
 }
 
 function uploadFile(cb){
     setTimeout(() => {
         console.log("Upload File Compmpleted");
+        cb()
     },1000)
 }
 
